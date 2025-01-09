0b5e79b157d2240bad25dfefce2a9dd77a5fc871
Aditya Iyer <aditya4iyer@gmail.com>
2025-01-08 18:57:18 -0800

    completed optim.py

--- optim.py
+++ optim.py
@@ -56,7 +56,7 @@ X = rate_of_return.T # Tranpose of rate_of_return
 X0 = X[0] #first price of X
 
 
-market_rate = close.T[0][-1] / close.T[0][0] - 1 # Current s&p 500 rate divided by original
+market_rate = close.T[0][-1] / close.T[0][0] - 1 # Current spy rate divided by original
 # print(market_rate)
 risk_free_rate = 0.055
 
@@ -97,14 +97,14 @@ def MinimizeRisk(beta, capm, target_return):
 
 
 
-def MaximizeReturn(beta, capm, target_return):
+def MaximizeReturn(beta, capm, target_beta):
 
 	beta, capm = np.array(beta), np.array(capm)
 
 	def objective(W):
-		return W @ beta
+		return - (W @ beta)
 	def constraint(W):
-		return W @ capm - target_return
+		return - (W @ beta - target_beta)
 	def constraint2(W):
 		return W @ np.ones(len(W)) - 1
 	def constraint3(W):
@@ -121,7 +121,34 @@ def MaximizeReturn(beta, capm, target_return):
 
 	return res.x
 
+minrisk, maxretn = MinimizeRisk(BETA, CAPM, 0.13), MaximizeReturn(BETA, CAPM, 1.4)
 
+fig = plt.figure(figsize=(13, 8))
+ax = fig.add_subplot(121)
+ay = fig.add_subplot(122)
+
+target_rtn = 0.13
+target_beta = 1.25
+
+dot = lambda x, y: np.sum([i*j for i, j in zip(x, y)])
+
+minimized_risk = dot(minrisk, BETA)
+maximized_return = dot(maxretn, CAPM)
+
+titleA = f'Minimized Risk Portfolio\nTargetCAPM: {target_rtn}\nPortfolio Beta: {round(minimized_risk, 3)}'
+titleB = f'Maximized Return Portfolio\nTargetBeta: {target_beta}\nPortfolio Return: {round(maximized_return, 3)}'
+
+
+ax.set_title(titleA)
+ay.set_title(titleB)
+
+s_tickers = [f'{i} | Beta: {round(j, 3)}' for i, j in zip(tickers, BETA)]
+
+ax.pie(minrisk, labels=s_tickers, autopct='%1.1f%%')
+ay.pie(maxretn, labels=s_tickers, autopct='%1.1f%%')
+
+
+plt.show()
 
 
 
